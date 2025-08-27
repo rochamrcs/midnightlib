e25e8bd89834763d16fdebbf279ffa303190dd68
rochamrcs <mavinicius95@gmail.com>
2025-08-27 14:30:38 -0300

    Adicionando o arquivo Schemas e o status code

--- midnightlib/app.py
+++ midnightlib/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from midnightlib.schemas import Message
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Bem-vindos(as) à MidnightLib System, onde sonhos e imaginação '
     'ganham forma através de palavras que farão você viajar pela mente dos melhores romancistas.'}
