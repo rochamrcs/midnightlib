c8ced279e8f4ccd4d011e3fa5f15cd11ecfca4dc
rochamrcs <mavinicius95@gmail.com>
2025-09-02 22:01:01 -0300

    Ajustes no teste de conexão com o banco de dados

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,6 @@
-from sqlalchemy import text
+from sqlalchemy import Select
 
 
 def test_db_connection(session):
-    result = session.execute(text("SELECT 1")).scalar()
+    result = session.execute(Select(1)).scalar()
     assert result == 1
